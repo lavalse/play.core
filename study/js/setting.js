f526e3322280248176d02d10b131df2de3dc410d
XU Cong <lavalselike@gmail.com>
2023-03-19 23:17:41 +0900

    update

--- study/js/setting.js
+++ study/js/setting.js
@@ -1,13 +1,23 @@
-const pattern = "abcdABCD";
+const pattern = ".A.B.C.D:!*...";
+const colors = ['mediumvioletred', 'gold', 'orange'];
+
+export const settings = {
+  backgroundColor: "black",
+  color:"gray",
+  fontSize:"2em"
+}
 
 export function main(coord, context, cursor, buffer) {
 	const x = Math.floor(cursor.x) 
 	const y = Math.floor(cursor.y)
   const index = coord.index % pattern.length
+  const colorIndex = coord.index % colors.length
 
 	if (coord.x == x && coord.y == y) return '┼'
 	if (coord.x == x) return '│'
 	if (coord.y == y) return '─'
-	return pattern[index];
+	return {
+    char: pattern[index],
+  }
 }
 
