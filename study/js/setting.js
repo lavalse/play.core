a9877bcb4d29589fdf5e2344f7e55b972ced5036
XU Cong <lavalselike@gmail.com>
2023-03-21 19:35:55 +0900

    update

--- study/js/setting.js
+++ study/js/setting.js
@@ -1,10 +1,18 @@
-const pattern = ".A.B.C.D:!*...";
+const pattern = "ABCDabcd##";
 const colors = ['mediumvioletred', 'gold', 'orange'];
 
 export const settings = {
   backgroundColor: "black",
   color:"gray",
-  fontSize:"2em"
+  fontSize:"2em",
+  renderer:"text",
+  allowSelect:"true"
+}
+
+export function pre(context){
+  if (context.frame % 10 == 0) {
+		console.log(context.metrics.aspect)
+	}
 }
 
 export function main(coord, context, cursor, buffer) {
@@ -13,9 +21,9 @@ export function main(coord, context, cursor, buffer) {
   const index = coord.index % pattern.length
   const colorIndex = coord.index % colors.length
 
-	if (coord.x == x && coord.y == y) return '┼'
-	if (coord.x == x) return '│'
-	if (coord.y == y) return '─'
+	if (coord.x == x && coord.y == y) return 'X'
+	if (coord.x == x) return '|'
+	if (coord.y == y) return '-'
 	return {
     char: pattern[index],
   }
